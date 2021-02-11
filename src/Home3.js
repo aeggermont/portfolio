157bcd543bb3ac25a22f716ba6065d5d26bad70c
Antonio Eggermont <antonioeggermont@Antonios-Mac-Pro.local>
2021-02-10 21:15:50 -0600

    first commit

---
+++ src/Home3.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import Navbar from './component/Navbar/Navbar';
+import ParticlesBanner from './component/Banner/ParticlesBanner';
+import About from './component/About';
+
+//import Skills from './component/About';
+
+
+
+
+import Portfolio from './component/Portfolio/Portfolio';
+import Testimonial from './component/Testimonial/Testimonial';
+
+import Blog from './component/Blog/Blog';
+import MapContainer from './component/Map';
+import Contact from './component/Contact';
+import Skill from './component/Skill/Skill';
+import Footer from './component/Footer';
+import jhonData from './component/jhonData';
+import aaeData  from './component/aaeData';
+
+/*
+import Service from './component/Service';
+<Service wClass="work_area_two" jhonData={jhonData}/>
+      
+import Clientslogo from './component/Clientslogo';
+<Clientslogo/>
+<Skill cClass="bg_w"/>
+ <Testimonial/>
+ <MapContainer/>
+*/
+
+export const Home3 = () => (
+  <div className="body_wrapper">
+      <Navbar mClass="dark_menu" mContainer="custome_container" mainlogo="logo2.png" stickylogo="logo.png"/>
+      <ParticlesBanner aaeData={aaeData}/>
+      <About aClass='about_area bg_color' aaeData={aaeData}/>
+      <Blog/>
+      <Portfolio pClass="bg_color"/>
+      
+      <Contact aaeData={aaeData}/>
+      <Footer aaeData={aaeData}/>
+  </div>
+)
+
+
---
+++ src/component/Blog/Blog.js
@@ -0,0 +1,23 @@
+import React , {Component} from 'react';
+import Sectiontitle from '../Banner/Sectiontitle';
+import BlogItems from '../Blog/BlogItems';
+import Reveal from 'react-reveal/Reveal/';
+class Blog extends Component{
+    render(){
+        return(
+            <section className="blog_area" id="blog">
+                <div className="container">
+                    <Sectiontitle Title="Current Projects" TitleP=""/>
+                    <Reveal effect="fadeInUp" duration={1000}>
+                        <div className="row">
+                            <BlogItems bTitle="Disney Parks, Products & Experiences" bDetails="Suspendisse in mattis neque, sed accu- msan erat. Maecenas eget metus dui. Vestibulum accumsan massa quam.." btnText="Read More" image="post-img1.jpg" Pdata=""/>
+                            <BlogItems bTitle="Intelligent Virtual Asistants for Mexico's Government" bDetails="Suspendisse in mattis neque, sed accu- msan erat. Maecenas eget metus dui. Vestibulum accumsan massa quam.." btnText="Read More" image="post-img2.jpg" Pdata=""/>
+                            <BlogItems bTitle="Monitoring Whale Behavior Using Power From The Sea" bDetails="Suspendisse in mattis neque, sed accu- msan erat. Maecenas eget metus dui. Vestibulum accumsan massa quam.." btnText="Read More" image="post-img3.jpg" Pdata=""/>
+                        </div>
+                    </Reveal>
+                </div>
+            </section>
+        )
+    }
+}
+export default Blog;
