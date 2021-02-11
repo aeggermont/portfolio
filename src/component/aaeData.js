90800936c7c9f06e4a3d469c3f39bf4965d8aa04
Antonio Eggermont <antonioeggermont@Antonios-Mac-Pro.local>
2021-02-10 23:11:47 -0600

    added personalization class

---
+++ src/component/aaeData.js
@@ -0,0 +1,100 @@
+const aaeData = {
+    "name": "Antonio Aranda Eggermont.",
+    "name2": "Antonio Aranda Eggermont.",
+    "role": "UX Engineer & Applications Developer,",
+    "ptext": "Creative UX Engineer & Software Developer.",
+    "socialLinks":[
+        {
+          "name": "Facebook",
+          "url": "http://www.linkedin.com/",
+          "className": "social_facebook"
+        },
+        {
+          "name":"twitter",
+          "url":"http://www.twitter.com/",
+          "className":"social_twitter"
+        },
+        {
+          "name":"googleplus",
+          "url":"http://www.googleplus.com",
+          "className":"social_googleplus"
+        },
+        {
+            "name":"pinterest",
+            "url":"http://www.pinterest.com",
+            "className":"social_pinterest"
+          }
+      ],
+      "aboutme": "About Me",
+      "aboutdetails": "My work experience and projects have been primarily focused on developing tools, Web applications, and computer system designs that have been used in the entertainment and higher education industries. A core aspect of my work and projects has been the interaction design of meaningful experiences for the users whom I develop and design. My interest in the field of UX focuses on user research, interaction design, design thinking, experience strategy, software prototyping, and human computer interaction.",
+      "service":[
+          {
+            "id" : 1,
+            "iconName": "icon-desktop",
+            "serviceTitle": "Web Design",
+            "sDetails" : "There are many variations of pssages of Lorm available, bu in some form, Lorem Ipsum is simply dummy text."
+          },
+          {
+            "id" : 2,
+            "iconName": "icon-pencil",
+            "serviceTitle": "Development",
+            "sDetails" : "There are many variations of pssages of Lorm available, bu in some form, Lorem Ipsum is simply dummy text."
+          },
+          {
+            "id" : 3,
+            "iconName": "icon-presentation",
+            "serviceTitle": "Marketing",
+            "sDetails" : "There are many variations of pssages of Lorm available, bu in some form, Lorem Ipsum is simply dummy text."
+          },
+          {
+            "id" : 4,
+            "iconName": "icon-linegraph",
+            "serviceTitle": "Strategy",
+            "sDetails" : "There are many variations of pssages of Lorm available, bu in some form, Lorem Ipsum is simply dummy text."
+          },
+          {
+            "id" : 5,
+            "iconName": "icon-gears",
+            "serviceTitle": "Support",
+            "sDetails" : "There are many variations of pssages of Lorm available, bu in some form, Lorem Ipsum is simply dummy text."
+          },
+          {
+            "id" : 6,
+            "iconName": "icon-tools",
+            "serviceTitle": "We’re experienced",
+            "sDetails" : "There are many variations of pssages of Lorm available, bu in some form, Lorem Ipsum is simply dummy text."
+          }
+      ],
+      "contact":[
+        {
+          "id": "1",
+          "title": "Contact Info",
+          menuItems: [
+            {
+              id: 1,
+              icon: 'icon_pin_alt',
+              text: 'Address: 1100 University Street Apt 1004, Seattle, WA USA',
+            },
+            {
+              id: 2,
+              icon: 'icon_phone',
+              text: 'Phone: +1.206.556.8918',
+            },
+            {
+              id: 3,
+              icon: 'icon_mail',
+              text: 'Email: antonio.eggermont@post.harvard.edu',
+            },
+            {
+              id: 4,
+              icon: 'icon_globe',
+              text: 'Website: http://example.com',
+            }
+          ]
+        },
+      ],
+    contacttitle: 'Contact Form',
+}
+
+export default aaeData;
+
